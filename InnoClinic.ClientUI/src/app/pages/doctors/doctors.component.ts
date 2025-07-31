a4f3b330d85e222a397dfbaba4f5fd1c53c5776e
Alex Tarski <Alex.F.L.O.W@yandex.ru>
2025-07-31 22:54:28 +0300

    Add doctor component
    Add doctor-card component; add doctor-service

--- InnoClinic.ClientUI/src/app/pages/doctors/doctors.component.ts
+++ InnoClinic.ClientUI/src/app/pages/doctors/doctors.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {DoctorCard} from "../../components/doctor-card/doctor-card";
 
 @Component({
   selector: 'app-doctors',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, DoctorCard],
   templateUrl: './doctors.component.html',
   styleUrl: './doctors.component.css'
 })
