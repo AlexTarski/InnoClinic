1720042aa1199f5674f095e0201e7c4e552583f2
Alex Tarski <Alex.F.L.O.W@yandex.ru>
2025-09-12 20:28:19 +0300

    feat(EmployeeUI): add office info view on the offices page
    When click on the row of the office
    the page with the office information is displayed
    Add office-no-photo picture

--- InnoClinic.EmployeeUI/src/app/components/office-card/office-card.ts
+++ InnoClinic.EmployeeUI/src/app/components/office-card/office-card.ts
@@ -1,12 +1,59 @@
-import {Component, Input} from '@angular/core';
+import {Component, Inject, Input} from '@angular/core';
+import {DIALOG_DATA, DialogRef} from "@angular/cdk/dialog";
 import {Office} from "../../data/interfaces/office.interface";
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-office-card',
-  imports: [],
-	templateUrl: `./office-card.component.html`,
+	imports: [
+		NgOptimizedImage
+	],
+	template: `
+		<div class="office-card">
+			<div class="office-photo">
+				<img ngSrc="/assets/imgs/office-no-photo.png" alt="SUCCESS!" width="350" height="350">
+			</div>
+				<div class="office-info-container">
+					<div class="office-info">
+						<h3>Office address:</h3>
+						<h4>
+							{{ office.office.address.city }},
+							{{ office.office.address.street }},
+							{{ office.office.address.houseNumber }},
+							office {{ office.office.address.officeNumber }}
+						</h4>
+					</div>
+					<div class="office-info">
+						<h3>Registry phone number:</h3>
+						<h4>{{ office.office.registryPhoneNumber }}</h4>
+					</div>
+					<div class="radioButtons">
+						<label>
+							<input type="radio" value=true [checked]="office.office.isActive" name="{{ office.office.id }}" disabled>
+							Active
+						</label>
+						<label>
+							<input type="radio" value=false [checked]="!office.office.isActive" name="{{ office.office.id }}" disabled>
+							Inactive
+						</label>
+					</div>
+					<button class="editOffice-btn">Edit</button>
+				</div>
+			<button class="close-btn" onclick="close()">×</button>
+		</div>
+	`,
 	styleUrl: `./office-card.component.css`
 })
 export class OfficeCard {
-	@Input() office?: Office;
+	constructor(
+			@Inject(DIALOG_DATA) public office:
+			{
+				office: Office
+			},
+			private dialogRef: DialogRef
+	) {}
+
+	close() {
+		this.dialogRef.close();
+	}
 }
--- InnoClinic.EmployeeUI/src/app/pages/offices/offices.component.ts
+++ InnoClinic.EmployeeUI/src/app/pages/offices/offices.component.ts
@@ -2,6 +2,8 @@ import {Component, inject} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {OfficeService} from "../../data/services/office.service";
 import {Office} from "../../data/interfaces/office.interface";
+import {Dialog} from "@angular/cdk/dialog";
+import {OfficeCard} from "../../components/office-card/office-card";
 
 @Component({
   selector: 'app-offices',
@@ -12,6 +14,7 @@ import {Office} from "../../data/interfaces/office.interface";
 })
 export class OfficesComponent {
   officeService = inject(OfficeService);
+	private dialog = inject(Dialog)
   offices: Office[] = [];
 
   constructor(){
@@ -22,4 +25,10 @@ export class OfficesComponent {
   }
 
 	createOffice(){}
+	openDialog(office: Office){
+		this.dialog.open(OfficeCard,
+				{
+					data: {office: office},
+				})
+	}
 }
